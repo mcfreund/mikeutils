e58a5b3cbf824faeeead5f0c09da7e3f98743039
mcfreund <m.freund@wustl.edu>
2020-01-15 15:00:36 -0600

    asdf

--- R/collate_surface_params.R
+++ R/collate_surface_params.R
@@ -3,7 +3,6 @@
 #' Optionally extracts sub-bricks by labels.
 #' @param v a numeric vector
 #' @param m a numeric vector or matrix
-#' @keywords scaling, normalizing, centering, linear algebra
 #' @examples
 
 collate_surface_params <- function(a) a
