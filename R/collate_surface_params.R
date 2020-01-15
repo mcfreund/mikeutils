357c6b35c5a2fbe817768729de453398959b7444
mcfreund <m.freund@wustl.edu>
2020-01-15 15:02:51 -0600

    asdf

--- R/collate_surface_params.R
+++ R/collate_surface_params.R
@@ -1,7 +1,8 @@
 #' Common vector math.
 #'
-#' @param v a numeric vector
-#' @param m a numeric vector or matrix
+#' @param name name of .gii file to read
+#' @param xlabels optional; a character vector that indicates the exact strings of the sub-bricks to be extracted. If omitted, all sub-bricks are extracted.
+#' @param space the surface space the .gii is in. \%in\% c("hcp", "fsave")
 #' @keywords scaling, normalizing, centering, linear algebra
 
 #' @export
