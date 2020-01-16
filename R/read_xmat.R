c9c5ce5912b9fc56cf75b99d199ee66fe654e63c
mcfreund <m.freund@wustl.edu>
2020-01-15 21:32:21 -0600

    added read_xmat

---
+++ R/read_xmat.R
@@ -0,0 +1,33 @@
+#' Read an AFNI gifti image and reshapes to matrix.
+#'
+#' Optionally extracts sub-bricks by labels.
+#'
+#' @param name name of .xmat.1D file to read
+#' @param uncensored boolean: read an uncensored file? defaults to TRUE, so nrows corresponds to other GLM matrices (Y, E).
+#' @keywords AFNI, neuroimaging
+
+#' @export
+read_xmat <- function(
+  name,
+  uncensored = TRUE
+  )
+  {
+
+  ## delete previously created files (afni will not overwrite)
+
+  unlink("X_temp.1D")
+  unlink("X_temp")
+
+  ## get in format for R to read
+
+  afni("1d_tool.py", "-infile X_temp.1D -censor_fill -write X_temp.1D")  ## write 1D file
+  afni("1dcat", "-d X_temp.1D > X_temp")  ## write text file from 1D file for R to read
+
+  X <- as.matrix(read.table("X_temp", quote = "\"", comment.char = ""))
+
+  unlink("X_temp.1D")
+  unlink("X_temp")
+
+  X
+
+}
