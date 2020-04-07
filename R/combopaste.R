2c535c844c3032f334d71ae9385d93c395d50dd0
mcfreund <m.freund@wustl.edu>
2020-04-07 10:55:05 -0500

    read behav add & combopaste update

--- R/combopaste.R
+++ R/combopaste.R
@@ -11,14 +11,19 @@
 #' combopaste3(letters[1:2], letters[3:4], letters[24:26])  ## accommodates upt to three vectors
 
 #' @export
-combo.paste <- function(a, b, sep = ".") apply(expand.grid(a, b), 1, paste0, collapse = sep)
+# combo.paste <- function(a, b, sep = ".") apply(expand.grid(a, b), 1, paste0, collapse = sep)
+combo.paste <- function(a, b, ..., sep = ".") apply(expand.grid(a, b, ...), 1, paste0, collapse = sep)
 #' @export
-combo_paste <- function(a, b, sep = "_") apply(expand.grid(a, b), 1, paste0, collapse = sep)
+# combo_paste <- function(a, b, sep = "_") apply(expand.grid(a, b), 1, paste0, collapse = sep)
+combo_paste <- function(a, b, ..., sep = "_") apply(expand.grid(a, b, ...), 1, paste0, collapse = sep)
 #' @export
-combopaste <- function(a, b, sep = "") apply(expand.grid(a, b), 1, paste0, collapse = sep)
+# combopaste <- function(a, b, sep = "") apply(expand.grid(a, b), 1, paste0, collapse = sep)
+combopaste <- function(a, b, ..., sep = "") apply(expand.grid(a, b, ...), 1, paste0, collapse = sep)
 #' @export
 combo.paste3 <- function(a, b, d, sep = ".") apply(expand.grid(apply(expand.grid(a, b), 1, paste0, collapse = sep), d), 1, paste0, collapse = sep)
 #' @export
 combo_paste3 <- function(a, b, d, sep = "_") apply(expand.grid(apply(expand.grid(a, b), 1, paste0, collapse = sep), d), 1, paste0, collapse = sep)
 #' @export
 combopaste3 <- function(a, b, d, sep = "_") apply(expand.grid(apply(expand.grid(a, b), 1, paste0, collapse = sep), d), 1, paste0, collapse = sep)
+
+
---
+++ R/read_dmcc_behav.R
@@ -0,0 +1,52 @@
+#' Reads trial-level behavioral data and event times for DMCC2 tasks.
+#'
+#' @param files (character) Which files to read? Must be either %in% "axcpt", "cuedts", "stern", "stroop", "subjsum"; or "all." Defaults to "all".
+#' @param use.boxr (boolean) Use boxr or boxdrive? When on freund's laptop, defaults to boxdrive, otherwise boxr.
+#' @param boxdrive.path (string) String indicating path to boxdrive. Defaults to path for freund's laptop.
+#' @keywords read, events, behavior
+
+#' @export
+read_dmcc_behav <- function(
+  files = c("all"),
+  use.boxr = if (Sys.info()["nodename"] == "CCP-FREUND") TRUE else FALSE,
+  boxdrive.path = "C:/Users/mcf/Box"
+  ) {
+
+  ## input validation
+
+  files <- tolower(files)
+  if (files == "all") files <- c("axcpt", "cuedts", "stern", "stroop", "subjsum")
+  if (!any(files %in% c("axcpt", "cuedts", "stern", "stroop", "subjsum"))) stop("bad file string")
+
+  if (!use.boxr) if (!dir.exists(boxdrive.path)) stop("boxdrive path does not exist")
+
+  ## read
+
+  l <- setNames(vector("list", length(files)), files)
+
+  if (use.boxr) {
+
+    boxr::box_auth()
+
+    if ("axcpt" %in% files) l[["axcpt"]]     <- boxr::box_read_csv(534174973987)
+    if ("cuedts" %in% files) l[["cuedts"]]   <- boxr::box_read_csv(534204743026)
+    if ("stern" %in% files) l[["stern"]]     <- boxr::box_read_csv(534194820188)
+    if ("stroop" %in% files) l[["stroop"]]   <- boxr::box_read_csv(534179635480)
+    if ("subjsum" %in% files) l[["subjsum"]] <- boxr::box_read(file_id = 534193095106, stringsAsFactor = FALSE)
+
+
+  } else {
+
+    path <- file.path(boxdrive.path, "Preprocessed_Data", "_wrangled")
+
+    if ("axcpt" %in% files) l[["axcpt"]]     <- data.table::fread(path, "dmcc2_behavior-and-events_axcpt.csv")
+    if ("cuedts" %in% files) l[["cuedts"]]   <- data.table::fread(path, "dmcc2_behavior-and-events_cuedts.csv")
+    if ("stern" %in% files) l[["stern"]]     <- data.table::fread(path, "dmcc2_behavior-and-events_sternberg.csv")
+    if ("stroop" %in% files) l[["stroop"]]   <- data.table::fread(path, "dmcc2_behavior-and-events_stroop.csv")
+    if ("subjsum" %in% files) l[["subjsum"]] <- data.table::fread(path, "summary", "dmcc2_all-runs-summary.csv")
+
+  }
+
+  l
+
+}
